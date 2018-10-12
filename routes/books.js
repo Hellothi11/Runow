53923a4758cd9cf7de304fb3c9f87f8568cde671
nguyen.tam.thi <nguyen.tam.thi@vn-macbook-0004.local>
2018-10-12 13:43:39 +0700

    add authentication for book request

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,14 @@
 let express = require('express');
 let router = express.Router();
 let controller = require('../controllers/books.js')
+const passport = require('passport')
 
-router.get('/list', controller.list);
-router.get('/add', (req, res, next) => {
+router.get('/list', passport.authenticationMiddleware(), controller.list);
+router.get('/add', passport.authenticationMiddleware(), (req, res, next) => {
     res.render('add_book');
 });
-router.post('/add', controller.add);
-router.get('/delete/:id', controller.delete);
-router.get('/edit/:id', controller.getEdit);
-router.post('/edit/:id', controller.edit);
+router.post('/add', passport.authenticationMiddleware(), controller.add);
+router.get('/delete/:id', passport.authenticationMiddleware(), controller.delete);
+router.get('/edit/:id', passport.authenticationMiddleware(), controller.getEdit);
+router.post('/edit/:id', passport.authenticationMiddleware(), controller.edit);
 module.exports = router;
